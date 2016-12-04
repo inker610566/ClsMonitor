ae927e2eba0a91dc08de726926c85d04b03ad21f
inker610566 <inker610566@gmail.com>
2016-12-04 08:08:32 +0800

    Add screenlockerform csproject

---
+++ ScreenLocker/ScreenLockForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenLocker
+{
+    public partial class ScreenLockForm : Form
+    {
+        public ScreenLockForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
